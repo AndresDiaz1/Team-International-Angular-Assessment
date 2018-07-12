cd64febe2e020ff26e5710e4960decf2e839bc63
AndresDiaz1 <andiazvar@gmail.com>
2018-07-11 21:39:59 -0500

    Added error spans to employees form fields

--- src/app/components/employee-form/employee-form.component.ts
+++ src/app/components/employee-form/employee-form.component.ts
@@ -10,11 +10,13 @@ export class EmployeeFormComponent implements OnInit {
 
   form: FormGroup;
   currentDate: Date;
+  hasPressedSaveEmployee: boolean;
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.currentDate = new Date();
+    this.hasPressedSaveEmployee = false;
     this.createForm();
   }
 
@@ -25,11 +27,22 @@ export class EmployeeFormComponent implements OnInit {
       country: [null, Validators.required],
       userName: [null, Validators.required],
       hireDate: [null, Validators.required],
-      status: [null, Validators.required],
+      status: [true, Validators.required],
       area: [null, Validators.required],
       jobTitle: [null, Validators.required],
       tipRate: [null, Validators.required],
     });
+  }
+
+  isFieldValid(field) {
+    return !this.form.get(field).valid && this.hasPressedSaveEmployee;
+  }
+
+  saveEmployee(post) {
+    this.hasPressedSaveEmployee = true;
+    if (this.form.valid) {
+
+    }
   }
 
 }
