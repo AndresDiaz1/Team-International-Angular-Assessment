15f4a4ecb25ddf15c8d7c8a75ebb96540fc09916
AndresDiaz1 <andiazvar@gmail.com>
2018-07-11 21:46:37 -0500

    Restored unit tests

--- src/app/components/employee-form/employee-form.component.spec.ts
+++ src/app/components/employee-form/employee-form.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EmployeeFormComponent } from './employee-form.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MaterialModule} from '../../material/material.module';
+import {AppRoutingModule} from '../../app.routing';
+import {HomeComponent} from '../../pages/home/home.component';
+import {EmployeesListComponent} from '../employees-list/employees-list.component';
 
 describe('EmployeeFormComponent', () => {
   let component: EmployeeFormComponent;
@@ -8,7 +13,8 @@ describe('EmployeeFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EmployeeFormComponent ]
+      declarations: [ EmployeeFormComponent, HomeComponent, EmployeesListComponent ],
+      imports: [ReactiveFormsModule, MaterialModule, AppRoutingModule],
     })
     .compileComponents();
   }));
