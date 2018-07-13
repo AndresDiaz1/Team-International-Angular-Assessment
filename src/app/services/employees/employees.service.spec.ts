88ce91105a5d487aa86f0245b106c5770ae78040
AndresDiaz1 <andiazvar@gmail.com>
2018-07-13 14:46:43 -0500

    Added unit test to employees service

--- src/app/services/employees/employees.service.spec.ts
+++ src/app/services/employees/employees.service.spec.ts
@@ -8,6 +8,18 @@ describe('EmployeesService', () => {
   let injector: TestBed;
   let employeesService: EmployeesService;
   let httpMock: HttpTestingController;
+  const dummyEmployees = [{
+    'id': 1,
+    'name': 'Giacomo Guilizoni',
+    'dob': '1978/03/21',
+    'country': 'Italy',
+    'username': 'Peldi',
+    'hireDate': '2017/10/01',
+    'status': false,
+    'area': 'services',
+    'jobTitle': 1,
+    'tipRate': 0
+  }];
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -25,21 +37,49 @@ describe('EmployeesService', () => {
   }));
 
   it('should get employees', () => {
-    const dummyEmployees = [{
-      'id': 1,
-      'name': 'Giacomo Guilizoni',
-      'dob': '1978/03/21',
-      'country': 'Italy',
-      'username': 'Peldi',
-      'hireDate': '2017/10/01',
-      'status': false,
-      'area': 'services',
-      'jobTitle': 1,
-      'tipRate': 0
-    }];
     employeesService.getEmployees().subscribe(employees => {
       expect(employees.length).toBe(1);
       expect(employees).toEqual(dummyEmployees);
     });
+
+    const req = httpMock.expectOne('http://localhost:3000/employees');
+    expect(req.request.method).toBe('GET');
+    req.flush(dummyEmployees);
+  });
+
+  it('should post employees', () => {
+    employeesService.addEmployee(dummyEmployees).subscribe(employees => {
+      expect(employees[0]['id']).toBe(1);
+      expect(employees).toEqual(dummyEmployees);
+    });
+
+    const req = httpMock.expectOne('http://localhost:3000/employees', 'post to API');
+    expect(req.request.method).toBe('POST');
+    req.flush(dummyEmployees);
+    httpMock.verify();
+  });
+
+  it('should put employees', () => {
+    employeesService.updateEmployee(dummyEmployees, 1).subscribe(employees => {
+      expect(employees[0]['id']).toBe(1);
+      expect(employees).toEqual(dummyEmployees);
+    });
+
+    const req = httpMock.expectOne('http://localhost:3000/employees/1', 'put to API');
+    expect(req.request.method).toBe('PUT');
+    req.flush(dummyEmployees);
+    httpMock.verify();
+  });
+
+  it('should delete employees', () => {
+    employeesService.deleteEmployee(1).subscribe(employees => {
+      expect(employees[0]['id']).toBe(1);
+      expect(employees).toEqual(dummyEmployees);
+    });
+
+    const req = httpMock.expectOne('http://localhost:3000/employees/1', 'put to API');
+    expect(req.request.method).toBe('DELETE');
+    req.flush(dummyEmployees);
+    httpMock.verify();
   });
 });
